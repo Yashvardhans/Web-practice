8b067758e070f673a23aab96434017c5bd1168ec
Yashvardhans <yashvardhans016@gmail.com>
2022-06-26 12:40:59 +0530

    Updates

---
+++ precache-manifest.aeb3d879b8d7ce805f0915d157c69e25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f94145f8fbb33a2006a2",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/css/main.745db810.chunk.css"
+  },
+  {
+    "revision": "f94145f8fbb33a2006a2",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/js/main.d95d8ec3.chunk.js"
+  },
+  {
+    "revision": "b650f527e5cd18723d9f",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/js/runtime~main.7c55eef2.js"
+  },
+  {
+    "revision": "7eda298c7e78941b98d9",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/js/2.2be6064f.chunk.js"
+  },
+  {
+    "revision": "b2871250ed127b512992",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/js/3.6f35523b.chunk.js"
+  },
+  {
+    "revision": "236beedc8566bd7b978710b049a13c13",
+    "url": "/Web-practice/netflix-clone/netfix-clone/static/media/banner.236beedc.png"
+  },
+  {
+    "revision": "392cc366c3ac03fd24ffdbe3335b5582",
+    "url": "/Web-practice/netflix-clone/netfix-clone/index.html"
+  }
+];
