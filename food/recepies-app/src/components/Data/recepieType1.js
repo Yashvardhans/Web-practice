5d49e79644066a27a35e5bb25ea0145f8fce8c89
Yashvardhans <yashvardhans016@gmail.com>
2022-07-17 01:37:19 +0530

    portfolio website

---
+++ food/recepies-app/src/components/Data/recepieType1.js
@@ -0,0 +1,285 @@
+const recepies = [
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    },
+    {
+        title : "Lorem ipsum dolor sit amet",
+        ingredients : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet" ,"Lorem ipsum dolor sit amet"
+               ,"Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet" , "Lorem ipsum dolor sit amet"],
+        process : [
+            {
+                step_title : "Step 1",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 2",
+                step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                 Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                 pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                step_title : "Step 3",
+                 step_process : "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\
+                Aenean ut tristique tellus. Ut ultrices auctor fermentum. Quisque justo tortor, \
+                pulvinar non pulvinar nec, feugiat id elit. Proin varius, tortor vitae ullamcorper consequat"
+            },
+            {
+                tips : ["Lorem ipsum dolor sit amet","Lorem ipsum dolor sit amet"]
+            }
+        ]
+    }
+
+];
+
+export default recepies ;
